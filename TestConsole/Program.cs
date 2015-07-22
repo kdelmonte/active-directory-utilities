e13ad433ff4fe248ea4603235f4a383e54e9e798
Kelvin Del Monte <kdelmonten@gmail.com>
2015-07-22 00:24:43 -0400

    Update test console project

--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -1,25 +1,34 @@
 ﻿using System;
 using ActiveDirectoryUtilities;
 
-namespace Test
+namespace TestConsole
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var domainName = "";
+            Console.WriteLine("Enter the domain name");
+            var domainName = Console.ReadLine();
 
             // Set credentials of the user account that we will use to perform the domain 
             // the active directory operations. This user must have access to the AD
-            var operatingUsername = "";
-            var operatingUserPassword = "";
+            Console.WriteLine("Enter the operating user name");
+            var operatingUsername = Console.ReadLine();
+            Console.WriteLine("Enter the operating user password");
+            var operatingUserPassword = Console.ReadLine();
 
             // Instantiate the class using the variables above
+            Console.WriteLine("Test reading entire Active Directory");
             var activeDirectory = new ActiveDirectory(domainName,operatingUsername,operatingUserPassword);
             activeDirectory.Populate(true);
 
             // Authenticate a user
-            if (activeDirectory.AuthenticateUser("", ""))
+            Console.WriteLine("Testing user authentication");
+            Console.WriteLine("Enter the user name");
+            var username = Console.ReadLine();
+            Console.WriteLine("Enter the password");
+            var password = Console.ReadLine();
+            if (activeDirectory.AuthenticateUser(username, password))
             {
                 Console.WriteLine("You have been authenticated");
             }
